9686ef9973c011258257d8dd4ef03922381751b6
FERNANDO TIBANA <tibana.fernando@gmail.com>
2020-08-11 15:41:34 -0500

    validacion y depuracion

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
 $servidor="mysql:dbname=tareas;host=localhost";
-$usuario="root";
-$password="sleeppartypeople";
+$usuario="test";
+$password="test";
 
 try{
     $pdo=new PDO($servidor,$usuario,$password,array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
